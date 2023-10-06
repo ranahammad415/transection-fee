42fd253895c00d43e7f0c50f60aae643ff1d455d
engineeringdloyal <enghammad@dloyal.com>
2023-10-06 22:02:05 +0500

    push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 
 const app = express();
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 8080);
 
 const browserP = puppeteer.launch({headless: true});
 
