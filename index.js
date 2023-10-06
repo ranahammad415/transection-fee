fe8071491c4c5fcae0da1f1a345220081106bc1f
engineeringdloyal <enghammad@dloyal.com>
2023-10-06 21:09:17 +0500

    push

--- index.js
+++ index.js
@@ -7,16 +7,7 @@ app.set("port", process.env.PORT || 5000);
 const browserP = puppeteer.launch({headless: true});
 
 app.get("/", (req, res) => {
-  // FIXME move to a worker task; see https://devcenter.heroku.com/articles/node-redis-workers
-  let page;
-  (async () => {
-    page = await (await browserP).newPage();
-    await page.setContent(`<p>web running at ${Date()}</p>`);
-    res.send(await page.content());
-  })()
-    .catch(err => res.sendStatus(500))
-    .finally(() => page.close())
-  ;
+ 
 });
 
 app.listen(app.get("port"), () => 
